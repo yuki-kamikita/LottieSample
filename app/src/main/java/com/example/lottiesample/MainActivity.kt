02eb6ac5fe81326a5edd896b0da0a9cb4954d37c
shuhei.kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-02-01 14:31:40 +0900

    ボタンを押すと1ループ動く

--- app/src/main/java/com/example/lottiesample/MainActivity.kt
+++ app/src/main/java/com/example/lottiesample/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lottiesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val anim = findViewById<LottieAnimationView>(R.id.animationView)
+            anim.visibility = View.VISIBLE
+            anim.playAnimation()
+        }
     }
 }
