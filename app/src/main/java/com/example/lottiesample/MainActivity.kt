252f57c1b5681c6852a3c43c0abdb54288dbb4f8
shuhei.kiyosawa <shuhei.kiyosawa@karadanote.jp>
2022-02-01 14:59:34 +0900

    コメント追加
    おまけでバイブ追加

--- app/src/main/java/com/example/lottiesample/MainActivity.kt
+++ app/src/main/java/com/example/lottiesample/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.lottiesample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.VibrationEffect.DEFAULT_AMPLITUDE
+import android.os.Vibrator
 import android.view.View
 import android.widget.Button
 import com.airbnb.lottie.LottieAnimationView
@@ -13,9 +17,22 @@ class MainActivity : AppCompatActivity() {
 
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener {
+            // おまけのバイブ
+            val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                val vibrationEffect = VibrationEffect.createOneShot(20, DEFAULT_AMPLITUDE)
+                vibrator.vibrate(vibrationEffect)
+            } else {
+                vibrator.vibrate(20)
+            }
+
+            // アニメーション
             val anim = findViewById<LottieAnimationView>(R.id.animationView)
             anim.visibility = View.VISIBLE
             anim.playAnimation()
         }
     }
 }
+
+// 公式ドキュメント: http://airbnb.io/lottie/#/android?id=animation-listeners
+// 無料アニメーション: https://lottiefiles.com/featured
